08ba8eb066d2bb24cefb9f561719bfde9010772e
neutrinomuon <neutrinomuon@gmail.com>
2023-02-23 11:04:15 +0000

    Added new README file

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ ext1 = Extension(  name='pyfluxconserving.flib',
                  )
     
 setup( name='pyfluxconserving',
-       version='0.0.3',
+       version='0.0.4',
        ext_modules=[ ext1 ],
        extra_compile_args=['-O3'],
        description='Flux-conserving legacy routines in Fortran and Python',
